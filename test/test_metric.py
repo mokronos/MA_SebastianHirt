11b577e98cbda9ba1743aec70cedaaa579794594
Sebastian Hirt <sebastian@hirts.de>
2023-01-31 03:43:55 +0100

    added docstrings

--- test/test_metric.py
+++ test/test_metric.py
@@ -1,8 +1,15 @@
-import unittest
+import unittest  # import unittest module
 
 
 class TestMetric(unittest.TestCase):
+    '''
+    Test metrics with some examples
+    '''
+
     def test_metric(self):
+        '''
+        Test metric
+        '''
 
         # import metric and test it
         self.assertEqual(1, 1)
