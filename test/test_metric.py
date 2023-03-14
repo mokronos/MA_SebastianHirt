bb8bc7a311272520385d27845408d9589a7924fd
Sebastian Hirt <sebastian@hirts.de>
2023-03-14 01:34:32 +0100

    Add tests for text error rate function

--- test/test_metric.py
+++ test/test_metric.py
@@ -1,16 +1,26 @@
 """Module providing testing"""
 import unittest
+from src import helpers
 
 
 class TestMetric(unittest.TestCase):
     '''
-    Test metrics with some examples
+    Test metrics
     '''
 
-    def test_metric(self):
+    def test_text_error_rate(self):
         '''
-        Test metric
+        Test text error rate
         '''
 
-        # import metric and test it
-        self.assertEqual(1, 1)
+        string1 = 'this is a test 2321'
+        string2 = 'this is a test 1321!'
+
+        with self.subTest(string1=string1, string2=string2):
+            self.assertEqual(helpers.text_error_rate(string1, string2), 0.1)
+
+        string1 = 'test         rs2'
+        string2 = 'test rs2'
+
+        with self.subTest(string1=string1, string2=string2):
+            self.assertEqual(helpers.text_error_rate(string1, string2), 0.5)
