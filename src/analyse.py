b53ebc1ada3fa7dd7a6c9179bcab57b73c08da54
Sebastian Hirt <sebastian@hirts.de>
2023-03-29 01:00:06 +0200

    modified for multiple algorithms

--- src/analyse.py
+++ src/analyse.py
@@ -7,10 +7,10 @@ import matplotlib.pyplot as plt
 
 
 # split functions into i/o and processing
-reference_dir = 'data/raw/ReferenceSCIs'
-compressed_dir = 'data/raw/DistortedSCIs'
-mos_path = 'data/raw/MOS_SCID.txt'
-labels_dir = 'labels/raw'
+ref_dir = 'data/raw/scid/ReferenceSCIs'
+dist_dir = 'data/raw/scid/DistortedSCIs'
+mos_path = 'data/raw/scid/MOS_SCID.txt'
+gt_dir = 'data/gt/scid'
 
 image_ext = '.bmp'
 label_ext = '.txt'
@@ -18,18 +18,21 @@ prefix = 'SCI'
 
 
 # compare to MOS of dataset, somehow
-data = pd.read_csv('results/ter.csv')
+data = pd.read_csv('results/ter2.csv')
 data.dropna(inplace=True)
-print(data[['ter_comp', 'mos']].corr())
+# print(data[['ter_comp', 'mos']].corr())
 
-prcc, scrcc, adj_mos = helpers.nonlinearfitting(list(data['ter']), list(data['mos']), max_nfev=3000)
-print('Pearson correlation coefficient: ', prcc)
-print('Spearman correlation coefficient: ', scrcc)
-print('Adjusted MOS: ', adj_mos)
-data['adj_mos'] = adj_mos
+# prcc, scrcc, adj_mos = helpers.nonlinearfitting(list(data['ter']), list(data['mos']), max_nfev=3000)
+# print('Pearson correlation coefficient: ', prcc)
+# print('Spearman correlation coefficient: ', scrcc)
+# print('Adjusted MOS: ', adj_mos)
+# data['adj_mos'] = adj_mos
 
-data.plot.scatter(x='adj_mos', y='ter')
-# plt.xlim(0, 100)
-# plt.ylim(0, 100)
+algos = ['tess', 'ezocr']
+
+ax = data.plot.scatter(x='mos', y=f'ter_comp_{algos[0]}', color='blue')
+data.plot.scatter(x='mos', y=f'ter_comp_{algos[1]}', ax=ax, color='red')
+plt.xlim(0, 100)
+plt.ylim(0, 100)
 plt.grid()
 plt.show()
