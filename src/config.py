ebc7063cf9134a67e252485a6b6b6f10b495746a
Sebastian Hirt <sebastian@hirts.de>
2023-04-24 11:44:33 +0200

    Add config file to have globally defined paths for data saving/loading

---
+++ src/config.py
@@ -0,0 +1,17 @@
+PATHS = {
+        # Path to the folder containing the images
+        "images_scid_dist": "data/raw/scid/DistortedSCIs",
+        "images_scid_ref": "data/raw/scid/ReferenceSCIs",
+        # Path to the folder containing the ground truth
+        "gt_line": "data/gt/scid/line",
+        "gt_para": "data/gt/scid/para",
+        # Path to the folder containing the predictions
+        "pred_ezocr_comp": "results/ezocr/comp",
+        "pred_ezocr_ref": "results/ezocr/ref",
+        "pred_tess_comp": "results/tess/comp",
+        "pred_tess_ref": "results/tess/ref",
+        # Path to the folder containing experiment images
+        "images_exp": "exp",
+        # Path to the folder containing result images
+        "analyze": "images/analyze",
+        }
