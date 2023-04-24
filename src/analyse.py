35b58cf80f205539b83f3bad3fd44a9d39627829
Sebastian Hirt <sebastian@hirts.de>
2023-04-24 13:15:29 +0200

    Add subplot version, Reverse colormap

--- src/analyse.py
+++ src/analyse.py
@@ -14,7 +14,7 @@ def plot():
     # plot
     markers = ['v', '2', 's', 'P', 'x', 'd', '.', 'p', '*']
     comps = ["GN", "GB", "MB", "CC", "JPEG", "JPEG2000", "CSC", "HEVC-SCC", "CQD"]
-    colormap = 'copper'
+    colormap = 'copper_r'
     # print(f'data:\n{data}')
     for num in data.img_num.unique():
         print(f'Plotting normal values for img {num}')
@@ -45,6 +45,7 @@ def plot():
         plt.savefig(f'images/analyze/mos_ter_ezocr_img{num}.pdf')
         plt.close()
 
+
 def plot_fitted():
     # compare to MOS of dataset, somehow
     data = pd.read_csv('results/cer_ezocr.csv')
@@ -53,13 +54,13 @@ def plot_fitted():
     # plot
     markers = ['v', '2', 's', 'P', 'x', 'd', '.', 'p', '*']
     comps = ["GN", "GB", "MB", "CC", "JPEG", "JPEG2000", "CSC", "HEVC-SCC", "CQD"]
-    colormap = 'copper'
+    colormap = 'copper_r'
     for num in data.img_num.unique():
         print(f'Plotting fitted values for img {num}')
         tmp = data.loc[(data.img_num == num) & (data.comp == 1)].copy()
-        _, _, fitted_tmp = helpers.nonlinearfitting(tmp['cer_comp'],
-                                                    tmp['mos'],
-                                                    max_nfev=10000)
+        fitted_tmp = helpers.nonlinearfitting(tmp['cer_comp'],
+                                              tmp['mos'],
+                                              max_nfev=10000)
         tmp['cer_comp_fitted'] = fitted_tmp
         ax = tmp.plot.scatter(x='mos',
                               y='cer_comp_fitted',
@@ -70,9 +71,9 @@ def plot_fitted():
         for idx, comp in enumerate(data.comp.unique()[1:], start=1):
             # print(f'Plotting for comp {comp}')
             tmp = data.loc[(data.img_num == num) & (data.comp == comp)].copy()
-            _, _, fitted_tmp = helpers.nonlinearfitting(tmp['cer_comp'],
-                                                        tmp['mos'],
-                                                        max_nfev=10000)
+            fitted_tmp = helpers.nonlinearfitting(tmp['cer_comp'],
+                                                  tmp['mos'],
+                                                  max_nfev=10000)
             tmp['cer_comp_fitted'] = fitted_tmp
             # print(f'data for comp {comp}:\n{tmp}')
             tmp.plot.scatter(x='mos',
@@ -91,6 +92,85 @@ def plot_fitted():
         plt.savefig(f'images/analyze/mos_ter_fit_ezocr_img{num}.pdf')
         plt.close()
 
+
+def plot_sub():
+    # compare to MOS of dataset, somehow
+    data = pd.read_csv('results/cer_ezocr.csv')
+    data.dropna(inplace=True)
+
+    # plot
+    markers = ['v', '2', 's', 'P', 'x', 'd', '.', 'p', '*']
+    comps = ["GN", "GB", "MB", "CC", "JPEG", "JPEG2000", "CSC", "HEVC-SCC", "CQD"]
+    colormap = 'copper_r'
+    # print(f'data:\n{data}')
+    for num in data.img_num.unique():
+        print(f'Plotting normal values for img {num}')
+        # define subplots
+        fig, axs = plt.subplots(3, 3, figsize=(10, 10))
+
+        for idx, (comp,ax) in enumerate(zip(data.comp.unique(), axs.flatten())):
+            # print(f'Plotting for comp {comp}')
+            tmp = data[(data.img_num == num) & (data.comp == comp)]
+            # print(f'data for comp {comp}:\n{tmp}')
+            tmp.plot.scatter(x='mos',
+                             y='cer_comp',
+                             c='qual',
+                             colormap=colormap,
+                             marker=markers[idx],
+                             ax=ax,
+                             colorbar=True)
+            ax.set_xlim(0, 100)
+            ax.set_ylim(0, 100)
+            ax.set_xlabel("$MOS$")
+            ax.set_ylabel("$1-CER$")
+            ax.set_title(f'{comps[idx]} ({comp})')
+        fig.suptitle(f'Image: {num}')
+        plt.tight_layout()
+        plt.savefig(f'images/analyze/mos_ter_ezocr_sub_img{num}.pdf')
+        plt.close()
+
+def plot_fitted_sub():
+    # compare to MOS of dataset, somehow
+    data = pd.read_csv('results/cer_ezocr.csv')
+    data.dropna(inplace=True)
+
+    # plot
+    markers = ['v', '2', 's', 'P', 'x', 'd', '.', 'p', '*']
+    comps = ["GN", "GB", "MB", "CC", "JPEG", "JPEG2000", "CSC", "HEVC-SCC", "CQD"]
+    colormap = 'copper_r'
+    # print(f'data:\n{data}')
+    for num in data.img_num.unique():
+        print(f'Plotting fitted values for img {num}')
+        # define subplots
+        fig, axs = plt.subplots(3, 3, figsize=(10, 10))
+
+        for idx, (comp,ax) in enumerate(zip(data.comp.unique(), axs.flatten())):
+            # print(f'Plotting for comp {comp}')
+            tmp = data.loc[(data.img_num == num) & (data.comp == comp)].copy()
+            fitted_tmp = helpers.nonlinearfitting(tmp['cer_comp'],
+                                                  tmp['mos'],
+                                                  max_nfev=10000)
+            tmp['cer_comp_fitted'] = fitted_tmp
+            # print(f'data for comp {comp}:\n{tmp}')
+            tmp.plot.scatter(x='mos',
+                             y='cer_comp_fitted',
+                             c='qual',
+                             colormap=colormap,
+                             marker=markers[idx],
+                             ax=ax,
+                             colorbar=True)
+            ax.set_xlim(0, 100)
+            ax.set_ylim(0, 100)
+            ax.set_xlabel("$MOS$")
+            ax.set_ylabel("$1-CER$")
+            ax.set_title(f'{comps[idx]} ({comp})')
+        fig.suptitle(f'Image: {num}')
+        plt.tight_layout()
+        plt.savefig(f'images/analyze/mos_ter_fit_ezocr_sub_img{num}.pdf')
+        plt.close()
+
 if __name__ == '__main__':
     plot()
     plot_fitted()
+    plot_sub()
+    plot_fitted_sub()
