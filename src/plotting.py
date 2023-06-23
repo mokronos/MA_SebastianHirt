48622ff26e6b5efdf6b20c22bd94a4123c46e076
mokronos <sebastian@hirts.de>
2023-06-23 05:50:24 +0200

    Add plotting functions

--- src/plotting.py
+++ src/plotting.py
@@ -138,46 +138,48 @@ def plot_sub():
             plt.clf()
             plt.close()
 
-def plot_fitted_sub():
+def plot_fitted_sub_single():
 
     data = pd.read_csv(PATHS['results_dist'])
     
     markers = ['v', '2', 's', 'P', 'x', 'd', '.', 'p', '*']
     colormap = 'copper_r'
 
-    grp_ocr = data.groupby('ocr_algo')
+    grp_target = data.groupby('target')
+    for name_target, group_target in grp_target:
+    
+        grp_ocr = group_target.groupby('ocr_algo')
+        for name_ocr, group_ocr in grp_ocr:
 
-    for name_ocr, group_ocr in grp_ocr:
+            grp_img = group_ocr.groupby('img_num')
+            grp_img = islice(grp_img, 1)
+            for name_img, group_img in grp_img:
 
-        grp_img = group_ocr.groupby('img_num')
-        grp_img = islice(grp_img, 1)
-        for name_img, group_img in grp_img:
+                grp_dist = group_img.groupby('dist') 
 
-            grp_dist = group_img.groupby('dist') 
+                fig, axs = plt.subplots(3, 3, figsize=(10, 10))
 
-            fig, axs = plt.subplots(3, 3, figsize=(10, 10))
-
-            for idx, ((name_dist, group_dist), ax) in enumerate(zip(grp_dist, axs.flatten())):
-                im = ax.scatter(group_dist['mos'],
-                                group_dist['cer_comp_fitted'],
-                                label=group_dist['dist_name'].iloc[0],
-                                marker=markers[idx],
-                                cmap=colormap,
-                                c=group_dist['qual'])
+                for idx, ((name_dist, group_dist), ax) in enumerate(zip(grp_dist, axs.flatten())):
+                    im = ax.scatter(group_dist['mos'],
+                                    group_dist['cer_comp_fitted'],
+                                    label=group_dist['dist_name'].iloc[0],
+                                    marker=markers[idx],
+                                    cmap=colormap,
+                                    c=group_dist['qual'])
 
 
-                fig.colorbar(im, ax=ax,label='Quality')
-                ax.set_xlabel("$MOS$")
-                ax.set_ylabel("$CER_{comp} (fitted)$")
-                ax.set_xlim(0, 100)
-                ax.set_ylim(0, 100)
-                ax.set_title(f'Dist. type: {group_dist["dist_name"].iloc[0]}')
+                    fig.colorbar(im, ax=ax,label='Quality')
+                    ax.set_xlabel("$MOS$")
+                    ax.set_ylabel("$CER_{comp} (fitted)$")
+                    ax.set_xlim(0, 100)
+                    ax.set_ylim(0, 100)
+                    ax.set_title(f'Dist. type: {group_dist["dist_name"].iloc[0]}')
 
-            # plt.savefig(f"images/mos_cer_fitted_sub_{name_ocr}_img{name_img}.pdf")
-            plt.tight_layout()
-            plt.show()
-            plt.clf()
-            plt.close()
+                # plt.savefig(f"images/mos_cer_{name_target}_fitted_sub_{name_ocr}_img{name_img}.pdf")
+                plt.tight_layout()
+                plt.show()
+                plt.clf()
+                plt.close()
 
 def plot_codec_comparison_cer():
 
@@ -291,6 +293,144 @@ def plot_codec_comparison_psnr():
         plt.close()
         # plt.show()
 
+def plot_cer_dist_quality():
+    """
+    CER_comp(against gt), seperate by dist type, highlight distortion quality, y(cer) x(qual)
+    """
+
+    # load dataframe
+    data = pd.read_csv(PATHS['results_dist'])
+
+
+    grp_target = data.groupby('target')
+    for name_target, group_target in grp_target:
+    
+        grp_ocr = group_target.groupby('ocr_algo')
+        for name_ocr, group_ocr in grp_ocr:
+
+            grp_dist = group_ocr.groupby('dist_name')
+            for name_dist, group_dist in grp_dist:
+
+                plt.plot(group_dist.groupby('qual')['cer_comp'].mean(),
+                         label=name_dist,
+                         marker='s')
+
+            plt.xlabel("Distortion quality")
+            plt.ylabel("$\overline{CER}_{comp}$")
+            plt.xticks(list(range(1, 6)))
+            plt.ylim(0, 100)
+            plt.grid()
+            plt.legend()
+            plt.tight_layout()
+            savepath_pdf=PATHS['analyze'](f'cer_dist_quality_{name_target}_{name_ocr}.pdf')
+            plt.savefig(savepath_pdf)
+            # savepath_png=PATHS['analyze'](f'cer_dist_quality_{name_target}_{name_ocr}.png')
+            # plt.savefig(savepath_png)
+            plt.title(f"Comparison of CER with target {name_target} for different distortion types with {name_ocr} OCR")
+            # plt.show()
+            plt.clf()
+            plt.close()
+
+def plot_fitted_total_sub_avg():
+
+    data = pd.read_csv(PATHS['results_dist'])
+    
+    markers = ['v', '2', 's', 'P', 'x', 'd', '.', 'p', '*']
+    colormap = 'copper_r'
+
+    crit = 'cer_comp_fitted_total'
+
+    grp_target = data.groupby('target')
+    for name_target, group_target in grp_target:
+    
+        grp_ocr = group_target.groupby('ocr_algo')
+        for name_ocr, group_ocr in grp_ocr:
+
+            fig, axs = plt.subplots(3, 3, figsize=(10, 10))
+
+            grp_dist = group_ocr.groupby('dist') 
+            for idx, ((name_dist, group_dist), ax) in enumerate(zip(grp_dist, axs.flatten())):
+
+                mean_dist = group_dist.groupby('qual')[[crit, 'mos']].mean().reset_index()
+
+                # plot mean cer/mos over images for each quality
+                im = ax.scatter(mean_dist['mos'],
+                                mean_dist[crit],
+                                label=group_dist['dist_name'].iloc[0],
+                                marker=markers[idx],
+                                cmap=colormap,
+                                c=mean_dist['qual'])
+
+
+                fig.colorbar(im, ax=ax,label='Quality')
+                ax.set_xlabel("$MOS$")
+                ax.set_ylabel("$CER_{comp} (fitted)$")
+                ax.set_xlim(0, 100)
+                ax.set_ylim(0, 100)
+                ax.set_title(f'Dist. type: {group_dist["dist_name"].iloc[0]}')
+                ax.grid()
+
+            plt.tight_layout()
+            savepath_pdf=PATHS['analyze'](f'cer_fitted_total_mos_{name_target}_{name_ocr}.pdf')
+            plt.savefig(savepath_pdf)
+            # savepath_png=PATHS['analyze'](f'cer_fittedtotal_mos_{name_target}_{name_ocr}.png')
+            # plt.savefig(savepath_png)
+            plt.title(f"Comparison of CER with target {name_target} for different distortion types with {name_ocr} OCR")
+            # plt.show()
+            plt.clf()
+            plt.close()
+
+
+def plot_sub_avg():
+
+    data = pd.read_csv(PATHS['results_dist'])
+    
+    markers = ['v', '2', 's', 'P', 'x', 'd', '.', 'p', '*']
+    colormap = 'copper_r'
+
+    crit = 'cer_comp'
+
+    grp_target = data.groupby('target')
+    for name_target, group_target in grp_target:
+    
+        grp_ocr = group_target.groupby('ocr_algo')
+        for name_ocr, group_ocr in grp_ocr:
+
+            fig, axs = plt.subplots(3, 3, figsize=(10, 10))
+
+            grp_dist = group_ocr.groupby('dist') 
+            for idx, ((name_dist, group_dist), ax) in enumerate(zip(grp_dist, axs.flatten())):
+
+                mean_dist = group_dist.groupby('qual')[[crit, 'mos']].mean().reset_index()
+
+                # plot mean cer/mos over images for each quality
+                im = ax.scatter(mean_dist['mos'],
+                                mean_dist[crit],
+                                label=group_dist['dist_name'].iloc[0],
+                                marker=markers[idx],
+                                cmap=colormap,
+                                c=mean_dist['qual'])
+
+
+                fig.colorbar(im, ax=ax,label='Quality')
+                ax.set_xlabel("$\overline{MOS}$")
+                ax.set_ylabel("$\overline{CER}_{comp} (fitted)$")
+                ax.set_xlim(0, 100)
+                ax.set_ylim(0, 100)
+                ax.set_title(f'Dist. type: {group_dist["dist_name"].iloc[0]}')
+                ax.grid()
+
+            plt.tight_layout()
+            savepath_pdf=PATHS['analyze'](f'cer_mos_{name_target}_{name_ocr}.pdf')
+            plt.savefig(savepath_pdf)
+            # savepath_png=PATHS['analyze'](f'cer_mos_{name_target}_{name_ocr}.png')
+            # plt.savefig(savepath_png)
+            plt.title(f"Comparison of CER with target {name_target} for different distortion types with {name_ocr} OCR")
+            # plt.show()
+            plt.clf()
+            plt.close()
+
+
 def image_diff():
     # calculate difference between reference and q=50 compressed image
     # load images
@@ -312,8 +452,11 @@ if __name__ == '__main__':
     # plot()
     # plot_sub()
     # plot_fitted()
-    plot_fitted_sub()
+    # plot_fitted_sub()
     # plot_codec_comparison_cer()
     # plot_codec_comparison_psnr()
     # image_diff()
+    # plot_cer_dist_quality()
+    # plot_fitted_total_sub_avg()
+    plot_sub_avg()
 
